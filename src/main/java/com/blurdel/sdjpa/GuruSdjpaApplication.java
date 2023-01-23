17e84c480964f6104de5a2452673a2759c655e5f
blurdel <5648737+blurdel@users.noreply.github.com>
2023-01-23 18:56:24 -0500

    initial commit

---
+++ src/main/java/com/blurdel/sdjpa/GuruSdjpaApplication.java
@@ -0,0 +1,13 @@
+package com.blurdel.sdjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruSdjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruSdjpaApplication.class, args);
+	}
+
+}
