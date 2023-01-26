1f6bc015c0486f7c8052d446a07305af4c6598e2
blurdel <5648737+blurdel@users.noreply.github.com>
2023-01-26 15:45:57 -0500

    Added database user creation scripts; Added unit tests- both regular and minimal context load

--- src/test/java/com/blurdel/sdjpa/GuruSdjpaApplicationTests.java
+++ src/test/java/com/blurdel/sdjpa/GuruSdjpaApplicationTests.java
@@ -1,13 +1,28 @@
 package com.blurdel.sdjpa;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.blurdel.sdjpa.repositories.BookRepository;
+
+@SpringBootTest // This does run Bootstrap (data-initializer)
 class GuruSdjpaApplicationTests {
+
+	@Autowired
+	private BookRepository bookRepository;
+	
+	
+	@Test
+	void testBookRepository() {
+		long count = bookRepository.count();
+		assertThat(count).isGreaterThan(0);
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
+	
 }
---
+++ src/test/java/com/blurdel/sdjpa/SpringBootJpaTestSplice.java
@@ -0,0 +1,47 @@
+package com.blurdel.sdjpa;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+
+import com.blurdel.sdjpa.domain.Book;
+import com.blurdel.sdjpa.repositories.BookRepository;
+
+
+//Brings up Hibernate with H2 database auto configured
+@DataJpaTest // Spring- Brings up minimal context for testing Spring data repositories (no Bootstrap (data-initializer), Web, JMS messaging)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class SpringBootJpaTestSplice {
+
+	@Autowired
+	BookRepository bookRepository;
+	
+	
+	@Commit // Default behavior would be to run this test, roll it back, then run the next test. Not for this particular test! 
+	@Order(1)
+	@Test
+	void testJpaSplice() {
+		long countBefore = bookRepository.count();
+		assertThat(countBefore).isEqualTo(0);
+		
+		bookRepository.save(new Book("My Book", "1235555", "Self"));
+		
+		long countAfter = bookRepository.count();
+		
+		assertThat(countBefore).isLessThan(countAfter);
+	}
+	
+	@Order(2)
+	@Test
+	void testJpaSpliceTransaction() {
+		long countBefore = bookRepository.count();
+		assertThat(countBefore).isEqualTo(1);
+	}
+	
+}
